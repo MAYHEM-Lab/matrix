914d6671fefbb39cfa72e24d457ef7c897c81b99
root <root@euca-10-1-4-161.eucalyptus.internal>
2018-02-08 14:32:20 -0800

    need this version

--- install-matrix.sh
+++ install-matrix.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-gunzip lapack-3.8.0.tar.gz
-tar -xf lapack-3.8.0.tar
+
+#!/bin/bash
+
+yum -y install blas blas-devel
+tar -xzf lapack-3.8.0.tar.gz
 cd lapack-3.8.0
+cp make.inc.example make.inc
 make blaslib
 make lapacklib -j 8
 cd LAPACKE
